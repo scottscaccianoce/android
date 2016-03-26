4d8480677449f2ec0f72f441db2812b1834cd35a
Scott Scaccianoce <scottscaccianoce@gmail.com>
2016-03-25 21:22:03 -0700

    Got hello world app working with screen rendered red

--- SapEngine/app/src/main/java/scottscaccianoce/sapengine/EngineActivity.java
+++ SapEngine/app/src/main/java/scottscaccianoce/sapengine/EngineActivity.java
@@ -1,5 +1,10 @@
 package scottscaccianoce.sapengine;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,24 +13,40 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class EngineActivity extends AppCompatActivity {
+    private GLSurfaceView glSurfaceView;
+    private boolean rendererSet = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_engine);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        glSurfaceView = new GLSurfaceView(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        final ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+
+        final boolean supportsEs2 = configurationInfo.reqGlEsVersion >= 0x20000
+                || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
+                && (Build.FINGERPRINT.startsWith("generic"))
+                || Build.FINGERPRINT.startsWith("unkown")
+                || Build.MODEL.contains("google_sdk")
+                || Build.MODEL.contains("Emulator")
+                || Build.MODEL.contains("Android DK built for x86"));
+
+        // Request an OpenGL ES 2.0 Compatiable Device
+        if (supportsEs2) {
+            glSurfaceView.setEGLContextClientVersion(2);
+            glSurfaceView.setEGLConfigChooser(8,8,8,8,16,0);
+            //Assign our renderer
+            glSurfaceView.setRenderer(new JavaRenderer());
+            rendererSet = true;
+        } else {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0", Toast.LENGTH_LONG).show();
+            return;
+        }
+        setContentView(glSurfaceView);
     }
 
     @Override
@@ -48,5 +69,18 @@ public class EngineActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(rendererSet) {
+            glSurfaceView.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
     }
 }
